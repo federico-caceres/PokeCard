a674740b3c804808a8533e10ad141782b8e7985d
Federico Cáceres <federicoenmanuel@gmail.com>
2023-02-27 21:24:42 -0300

    v1.2 Poke App

--- src/components/PokeHead/index.js
+++ src/components/PokeHead/index.js
@@ -1,6 +1,7 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Badge from 'react-bootstrap/Badge';
+import Image from 'react-bootstrap/Image'
 import './PokeHead.css';
 
 export default function PokeHead({searchPokemon}){
@@ -8,7 +9,12 @@ export default function PokeHead({searchPokemon}){
     return (
         <Row>
             <Col sm={12} md={4} className="d-flex justify-content-center align-items-center">
-                <h1><Badge bg="primary">PokeCard</Badge></h1>
+                <h1>
+                    <Badge bg="primary">
+                        <Image src='../../../poke-ball.png' width="40" className='m-1'></Image>
+                        PokeCard
+                    </Badge>
+                </h1>
             </Col>
             <Col sm={12} md={8} className="d-flex justify-content-center align-items-center">
                 <input className="search" placeholder='Ingresa el nombre del Pokémon a buscar' onChange={searchPokemon}></input>
